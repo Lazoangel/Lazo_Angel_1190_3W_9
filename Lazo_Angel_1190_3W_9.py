c7c0d0b9d7727bf036c9d28f5f5cb6dfa1893203
Lazoangel <a.23308051281190@cbtis128.edu.mx>
2024-09-27 09:40:19 -0600

    Add files via upload

---
+++ Lazo_Angel_1190_3W_9.py
@@ -0,0 +1,7 @@
+print ("Lazo Jimenez Angel Jesus 3W")
+Num_Ent = int (input("ingresar un numero entero: ")) #le pide al usuario ingresar un numero entero
+if Num_Ent > 40 and Num_Ent %7 == 0: #Esta linea verifica si el numero es mayor a 40 y es divisible entre 7
+    print (f"el numero {Num_Ent} es mayor a 40 y divisible entre 7") #Este mensaje aparece si el numero ingresado cumple las condiciones
+else:
+    print (f"el numero {Num_Ent} no cumple las condiciones") #Este mensaje aparece si el numero no cumple las condiciones
+    
